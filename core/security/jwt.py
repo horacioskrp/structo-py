a666075fe548a313b1d2efed78e1fc60260cd559
horacioskrp <hervekudayah@gmail.com>
2024-07-13 02:16:53 +0000

    🔒 adding python-jose and encode, decode & expired function to JWTHAndler class

---
+++ core/security/jwt.py
@@ -0,0 +1,52 @@
+from datetime import datetime, timedelta, timezone
+
+
+from jose import ExpiredSignatureError, JWTError, jwt
+
+from core.config import config
+from core.exceptions import CustomException
+
+class JWTDecodeError(CustomException):
+    code = 401
+    message = "Invalide token"
+
+class JWTExpiredError(CustomException):
+    code= 401
+    message ="Token expired"
+
+class JWTHandler:
+    secret_key = config.SECRET_KEY
+    algorithm = config.JWT_ALGORITHM
+    expire_minute = config.JWT_EXPIRE_MINUTES
+
+    @staticmethod
+    def encode(payload: dict) -> str:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=JWTHandler.expire_minute)
+        payload.update({"exp": expire})
+        return jwt.encode(
+            payload, JWTHandler.secret_key, algorithm=[JWTHandler.algorithm]
+        )
+    
+    @staticmethod
+    def decode(token: str) -> dict:
+        try:
+            return jwt.decode(
+                token, JWTHandler.secret_key, algorithms=[JWTHandler.algorithm]
+            )
+        except ExpiredSignatureError as exception:
+            raise JWTExpiredError() from exception
+        except JWTError() as exception:
+            raise JWTDecodeError() from exception
+    
+    @staticmethod
+    def decode_expired(token: str) -> dict:
+        try:
+            return jwt.decode(
+                token,
+                JWTHandler.secret_key,
+                algorithms=[JWTHandler.algorithm],
+                options={"verify_exp": False},
+            )
+        except JWTError as exception:
+            raise JWTDecodeError() from exception
+
